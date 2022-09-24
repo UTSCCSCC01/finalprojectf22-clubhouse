2855d326c02625461921a4f05bed898919536f60
Faraz Kaleem Malik <farazmalik17465@gmail.com>
2022-09-23 23:20:42 -0400

    Demo connection between frontend and backend

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 
 
+
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:5001/record").then( response => response.text()).then(body => this.setState({data: JSON.parse(body)})); // setState remounts the comp
   }
 
   render() {
     return (
-      <h1>Hello World!</h1>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Position</th>
+          <th>Level</th>
+        </tr>
+      {this.state.data.map( (v,i) => (
+        <tr key={v._id}>
+          <td>{v.name}</td>
+          <td>{v.position}</td>
+          <td>{v.level}</td>
+        </tr>
+      ))}
+      </table>
     );
   }
 }
