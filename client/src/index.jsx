d7dce228df8edac66706e455ba9541ff243d9b67
Faraz Kaleem Malik <farazmalik17465@gmail.com>
2022-10-02 15:19:36 -0400

    Add Roboto Font for Material UI

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -13,6 +13,9 @@ import RegisterForm from './pages/RegisterForm.jsx'
 ReactDOM.render(
   (
     <Router>
+      <Helmet>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      </Helmet>
       {/* Add global elements like navbar outside the switch */}
       <Routes>
         <Route exact path="/" element={<Home />} />
