41cdcb3a8161578f7fe7f6833c7075b1ba094cd4
Noah Cristino <noah.cristino@mail.utoronto.ca>
2022-10-02 21:19:52 -0400

    (MIN-2) Finished Basic Login Functionality

--- server/server.js
+++ server/server.js
@@ -6,9 +6,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
+app.use(require("./routes/login"));
 // get driver connection
 const dbo = require("./db/conn");
- 
+
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
