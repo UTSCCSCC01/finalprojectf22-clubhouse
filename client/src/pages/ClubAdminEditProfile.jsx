657cdd7e61f02aeb5892ddc578f250f4029ba489
d-p35 <dhruv10203@gmail.com>
2022-10-07 02:58:18 -0400

    (MIN 10) fixed typo

--- client/src/pages/ClubAdminEditProfile.jsx
+++ client/src/pages/ClubAdminEditProfile.jsx
@@ -137,7 +137,7 @@ const [open, setOpen] = useState(false);
             
             width: "100%",
             height: "100%",
-            position: "acsolute"}}
+            position: "absolute"}}
             />
           <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
         <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
