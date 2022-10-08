142cc3de5f70ca43d0bf207df089bd7984c0aa2c
amym-li <amyamay22@gmail.com>
2022-10-07 20:46:02 -0400

    Revert "Revert "Merge pull request #10 from UTSCCSCC01/MIN-6-upcoming-events""

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,6 +11,8 @@ import Home from './pages/Home.jsx';
 import CreateEvent from './pages/CreateEvent.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import TestLogin from './pages/TestLogin.jsx';
+import Navbar from './pages/NavBar.jsx';
+import Events from './pages/Events.jsx';
 import RegisterForm from './pages/RegisterForm.jsx'
 
 ReactDOM.render(
@@ -19,6 +21,7 @@ ReactDOM.render(
       <Helmet>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       </Helmet>
+      <Navbar />
       {/* Add global elements like navbar outside the switch */}
       <Routes>
         <Route exact path="/" element={<Home />} />
@@ -26,6 +29,7 @@ ReactDOM.render(
         <Route path="/login" element={<LoginPage />} />
         <Route path="/testlogin" element={<TestLogin />} />
         <Route path="/register" element={<RegisterForm />} />
+        <Route path="/events.html" element={<Events />} />
       </Routes>
     </Router>
   ),
