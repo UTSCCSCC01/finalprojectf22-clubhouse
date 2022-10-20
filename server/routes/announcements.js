ffb3f5e4928a101a71270be0a894606e69957689
amym-li <amyamay22@gmail.com>
2022-10-20 00:14:00 -0400

    MIN-8 Document announcement api endpoints

--- server/routes/announcements.js
+++ server/routes/announcements.js
@@ -1,58 +1,87 @@
 const express = require("express");
 const announcementRoutes = express.Router();
 const dbo = require("../db/conn");
- const ObjectId = require("mongodb").ObjectId;
- 
- 
-// This section will help you get a list of all the announcements.
-announcementRoutes.route("/announcements").get(function (req, res) {
- let db_connect = dbo.getDb("main");
- db_connect
-   .collection("announcements")
-   .find({})
-   .toArray(function (err, result) {
-     if (err) throw err;
-     res.json(result);
-   });
-});
+const ObjectId = require("mongodb").ObjectId;
 
-// This section will help you get a single announcement by id
-announcementRoutes.route("/announcements/:id").get(function (req, res) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  db_connect
-    .collection("announcements")
-    .findOne(myquery, function (err, result) {
+announcementRoutes.route("/announcements").get(
+
+  /**
+     * Endpoint that gets a list of all announcements
+     * @param {express.Request} req 
+     * @param {express.Response} res 
+     */
+
+  function (req, res) {
+    let db_connect = dbo.getDb("main");
+    db_connect
+      .collection("announcements")
+      .find({})
+      .toArray(function (err, result) {
+        if (err) throw err;
+        res.json(result);
+      });
+  });
+
+
+announcementRoutes.route("/announcements/:id").get(
+
+  /**
+     * Endpoint that gets a single announcement by id
+     * @param {express.Request} req 
+     * @param {express.Response} res 
+     */
+
+  function (req, res) {
+    let db_connect = dbo.getDb();
+    let myquery = { _id: ObjectId(req.params.id) };
+    db_connect
+      .collection("announcements")
+      .findOne(myquery, function (err, result) {
+        if (err) throw err;
+        res.json(result);
+      });
+  });
+
+announcementRoutes.route("/announcements/new").post(
+
+  /**
+     * Endpoint that creates a new announcement
+     * @param {express.Request} req 
+     * @param {express.Response} res 
+     */
+
+  function (req, response) {
+    let db_connect = dbo.getDb();
+    let myobj = {
+      clubName: req.body.clubName,
+      subject: req.body.subject,
+      message: req.body.message,
+      recipients: req.body.recipients,
+      time: req.body.time,
+    };
+    db_connect.collection("announcements").insertOne(myobj, function (err, res) {
       if (err) throw err;
-      res.json(result);
+      response.json(res);
     });
-});
-
-// This section will help you create a new announcement.
-announcementRoutes.route("/announcements/new").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myobj = {
-    clubName: req.body.clubName,
-    subject: req.body.subject,
-    message: req.body.message,
-    recipients: req.body.recipients,
-    time: req.body.time,
-  };
-  db_connect.collection("announcements").insertOne(myobj, function (err, res) {
-    if (err) throw err;
-    response.json(res);
   });
-});
 
-// This section will help you delete an announcement
-announcementRoutes.route("/:id").delete((req, response) => {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  db_connect.collection("announcements").deleteOne(myquery, function (err, obj) {
-    if (err) throw err;
-    console.log("1 document deleted");
-    response.json(obj);
+
+announcementRoutes.route("/:id").delete(
+
+  /**
+     * Endpoint that deletes a single announcement given an id
+     * @param {express.Request} req 
+     * @param {express.Response} res 
+     */
+
+  (req, response) => {
+    let db_connect = dbo.getDb();
+    let myquery = { _id: ObjectId(req.params.id) };
+    db_connect.collection("announcements").deleteOne(myquery, function (err, obj) {
+      if (err) throw err;
+      console.log("1 document deleted");
+      response.json(obj);
+    });
   });
-});
- 
+
 module.exports = announcementRoutes;
