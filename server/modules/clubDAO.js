e5304d764c81b61ff2be592a63e137858cc41924
Noah Cristino <noah.cristino@mail.utoronto.ca>
2022-10-27 18:58:05 -0400

    MIN-26 Added clubname detection and finished login status endpoint

---
+++ server/modules/clubDAO.js
@@ -0,0 +1,29 @@
+const { ModuleFilenameHelpers } = require("webpack");
+const dbo = require("../db/conn");
+/**
+ * @module modules/clubDAO
+ */
+
+
+/**
+ * Find a club in the database
+ * @param {String} email
+ * @returns {Promise<Object>} Object returned from .findOne() call - the club object itself
+ */
+ module.exports.findClub = function (email) {
+    let db_connect = dbo.getDb();
+
+    let clubObj = {
+        email: email
+    }
+
+    return new Promise( (res, rej) => {
+        db_connect.collection("clubs").findOne(clubObj, (err, result) => {
+            if (err) {
+                rej(err)
+            } else {
+                res(result)
+            }
+        });
+    })
+}
