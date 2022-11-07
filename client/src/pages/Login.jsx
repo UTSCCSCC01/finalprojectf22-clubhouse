a32e7170f0037e8522726f767b3909c7a4b75a64
d-p35 <dhruv10203@gmail.com>
2022-11-07 00:13:59 -0500

    (MIN 30) Fixed navbar spacing on login and register page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -74,7 +74,7 @@ class Login extends Component {
   render() {
     return (
       <div>
-      <Typography style={{paddingTop: "200px", paddingBottom: 20}} variant="h2" align="center">Login</Typography>
+      <Typography style={{paddingBottom: 20}} variant="h2" align="center">Login</Typography>
       <form onSubmit={this.submit}>
       <Container>
       <Grid container direction="column" style={{textAlign: "center"}}>
