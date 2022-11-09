eca1ebd313907970eea66e9d3196c8f80fd050a6
d-p35 <dhruv10203@gmail.com>
2022-11-08 20:16:33 -0500

    (MIN 30) Create different nav bars for different types of users and implement its functionality, also implement logout button functionality

--- client/src/pages/NavBar.jsx
+++ client/src/pages/NavBar.jsx
@@ -1,12 +1,33 @@
-import React, { Component, useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { AppBar, Tabs, Tab, Toolbar, Button, Box, Grid} from '@mui/material'
+import React, { Component, useState, useEffect} from 'react';
+import { AppBar, Tabs, Tab, Toolbar, Button, Box, Grid, Link, } from '@mui/material'
 import NotificationMenu from '../components/NotificationMenu.jsx';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import config from  '../../config.json'
 import dayjs from 'dayjs';
+import PropTypes from 'prop-types';
 import { getCookie } from '../libraries/cookieDAO.js';
+import Typography from '@mui/material/Typography';
+import {usePath} from '@mui/material';
+import {
+  MemoryRouter,
+  Route,
+  Routes,
+  matchPath,
+  useLocation,
+} from 'react-router-dom';
+import { StaticRouter } from 'react-router-dom/server';
 
+function LinkTab(props) {
+  return (
+    <Tab
+      component="a"
+      onClick={(event) => {
+        event.preventDefault();
+      }}
+      {...props}
+    />
+  );
+}
 /**
  * Create navbar and change color
  * when selected
@@ -19,7 +40,8 @@ import { getCookie } from '../libraries/cookieDAO.js';
       this.state = {
         menuAnchorEl: null,
         value: 0,
-        notifs: []
+        notifs: [],
+        account: ""
       }
 
       this.menuHandler = function(e) {
@@ -34,6 +56,7 @@ import { getCookie } from '../libraries/cookieDAO.js';
     }
 
     componentDidMount() {
+      this.state.account = getCookie("accountType");
       const notifBody = {
         timespan: 604800, // 1 week
         email: getCookie("username")
@@ -48,42 +71,140 @@ import { getCookie } from '../libraries/cookieDAO.js';
       fetch(config.api_url + "notif/get", fetchOptions).then(res => res.json()).then(res => this.setState({notifs: res}));
     }
 
+    handleLogout = () => {
+    // logout();
+    // location.href = "http://localhost:5001/logout";
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "accountType=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "clubName=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    location.href = "http://localhost:3000/allclubs";
+    console.log("logout");
+    }
+    
+
+    handleChange = (event, newValue) => {
+      this.setState({value: newValue});
+    };
+
+
+  Router(props) {
+  const { children } = props;
+  if (typeof window === 'undefined') {
+    return <StaticRouter location="/drafts">{children}</StaticRouter>;
+  }
+
+  return (
+    <MemoryRouter initialEntries={['/drafts']} initialIndex={0}>
+      {children}
+    </MemoryRouter>
+  );
+}
+
+
+useRouteMatch = (patterns) => {
+  const pathname = window.location.href;
+  console.log(pathname);
+
+  for (let i = 0; i < patterns.length; i += 1) {
+    const pattern = patterns[i];
+    const fullPattern = 'http://localhost:3000'+pattern;
+    console.log(fullPattern);
+    if (pathname==fullPattern) {
+      return i;
+    }
+  }
+
+  console.log("no match");
+  return null;
+}
+
     asStudent = () => {
+      const routes = [ '/home', '/allclubs', '/events', '/positions','/MyAccount'];
+      const index = this.useRouteMatch(routes);
+      
+      this.state.value = (index!=null) ? index : 0;
+      // this.state.value = 2;
+      console.log(this.state.value);
       return (
         <AppBar sx={{ background: '#002A5C' }}>
             <Toolbar>
-                <Tabs sx = {{ marginLeft: 'auto' }} textColor="inherit" value={this.state.value} onChange={(e, value) => setState({value})} indicatorColor="primary">
-                    <Tab label="Home"></Tab>
-                    <Tab label="Clubs"></Tab>
-                    <Tab label="Events"></Tab>
-                    <Tab label="Job Postings"></Tab>
-                    <Tab label="My account"></Tab>
+                <Tabs sx = {{ marginLeft: 'auto' }}  textColor="inherit" value={this.state.value} onChange={this.handleChange} indicatorColor="primary">
+
+                    <Tab label="Home" value={0} href={routes[0]}/>
+                    <Tab label="Clubs" value={1} href={routes[1]}/>
+                    <Tab label="Events" value={2} href={routes[2]}/>
+                    <Tab label="Job Postings" value={3} href={routes[3]}/>
+                    {/* <LinkTab label="Job Postings" href='/positions'/> */}
+                    <Tab label="My Account" value={4} href={routes[4]} />
                 </Tabs>
                   <Button sx={{ marginLeft: 'auto' }} variant="contained" onClick={this.menuHandler}><NotificationsActiveIcon /></Button>
                   <NotificationMenu anchorEl={this.state.menuAnchorEl} onClose={this.menuHandler} notifs={this.state.notifs}/>
-                  <Button sx={{ marginLeft: '10px' }} variant="contained"><NavLink to={"/login"}>Login</NavLink></Button>
-                  <Button sx={{ marginLeft: '10px' }} variant="contained"><NavLink to={"/register"}>Register</NavLink></Button>
+                  <Button sx={{ marginLeft: '10px' }} variant="contained" onClick={this.handleLogout}> Logout</Button>
             </Toolbar>
         </AppBar>
       )
     }
 
     asClub = () => {
+      const routes = ['/clubMain', '/new-announcement', '/create-event','/club-admin-profile'];
+      const index = this.useRouteMatch(routes);
+      
+      this.state.value = (index!=null) ? index : 0;
 
+      console.log(this.state.value);
+      return (
+        <AppBar sx={{ background: '#002A5C' }}>
+            <Toolbar>
+                <Tabs sx = {{ marginLeft: 'auto' }}  textColor="inherit" value={this.state.value} onChange={this.handleChange} indicatorColor="primary">
+
+                    <Tab label="Home" value={0} href={routes[0]}/>
+                    <Tab label="Make an Accouncement" value={1} href={routes[1]}/>
+                    <Tab label="Create an Event" value={2} href={routes[2]}/>
+                    <Tab label="Profile" value={3} href={routes[3]}/>
+                   
+                </Tabs>
+                  <Button sx={{ marginLeft: 'auto' }} variant="contained" onClick={this.handleLogout}> Logout</Button>
+            </Toolbar>
+        </AppBar>
+      )
     }
+    
 
     asAdmin = () => {
+      const routes = ['/manageClubs', '/clubRequests'];
+      const index = this.useRouteMatch(routes);
+      
+      this.state.value = (index!=null) ? index : 0;
+
+      console.log(this.state.value);
+      return (
+        <AppBar sx={{ background: '#002A5C' }}>
+            <Toolbar>
+                <Tabs sx = {{ marginLeft: 'auto' }}  textColor="inherit" value={this.state.value} onChange={this.handleChange} indicatorColor="primary">
+
+                    <Tab label="Manage Clubs" value={0} href={routes[0]}/>
+                    <Tab label="Club Requests" value={1} href={routes[1]}/>
+                </Tabs>
+                  <Button sx={{ marginLeft: 'auto' }} variant="contained" onClick={this.handleLogout}> Logout</Button>
+            </Toolbar>
+        </AppBar>
+      )
     }
 
     notLoggedIn = () => {
+
+      const routes = [ '/allclubs', '/events'];
+      const index = this.useRouteMatch(routes);
+      
+      this.state.value = (index!=null) ? index : 0;
       return (
         <AppBar sx={{ background: '#002A5C' }}>
             <Toolbar>
             <Grid container direction='row' justifyContent="flex-end" alignItems="center" >
                 <Grid item xs={4.75}>
-                <Tabs textColor="inherit" value={this.state.value} onChange={(e, value) => setState({value})} indicatorColor="primary">
-                    <Tab label="Clubs"></Tab>
-                    <Tab label="Events"></Tab>
+                <Tabs textColor="inherit" onChange={this.handleChange} value={this.state.value} indicatorColor="primary">
+                    <Tab label="Clubs" href='/allclubs'></Tab>
+                    <Tab label="Events" href='/events'></Tab>
                 </Tabs>
                 </Grid>
                 <Grid item xs={2}>
@@ -101,13 +222,14 @@ import { getCookie } from '../libraries/cookieDAO.js';
         </AppBar>
       )
     }
-    
 
+   
+
+ 
     render() {
-      return (
-        this.notLoggedIn()
-      )
+      return (this.state.account=="student")? (this.asStudent()) : (this.state.account=="club")?(this.asClub()) : (this.state.account=="admin")?(this.asAdmin()) : (this.notLoggedIn());
     }
   }
+
 
 export default Navbar;
