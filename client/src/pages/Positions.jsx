85e41d7c1b6abe660b06d4d80e74dc5f3bf27d7d
amym-li <amyamay22@gmail.com>
2022-11-14 18:16:48 -0500

    MIN-33 Create collapsing display for job information

--- client/src/pages/Positions.jsx
+++ client/src/pages/Positions.jsx
@@ -1,18 +1,11 @@
 import React, { Component } from "react";
 import axios from 'axios';
 import PositionCard from "./PositionCard.jsx";
-import { useEffect,useState } from 'react';
-import Box from '@mui/material/Box';
+
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import ListItemText from '@mui/material/ListItemText';
-import { PostAddSharp } from "@mui/icons-material";
+
 import '../styles.css';
 
 /**
@@ -55,7 +48,13 @@ class Positions extends Component{
     return positions.map((position, index) => (
       
       <Grid item sx={{ m: 2 }} key={index}>
-        <PositionCard key={index} eImage={position.clubImage} ePosition={position.jobPosition} cName={position.clubName} eMail={position.email}></PositionCard>
+        <PositionCard key={index}
+         jobDesc={position.jobDescription} 
+         jobReqs={position.jobRequirements} 
+         eImage={position.clubImage} 
+         ePosition={position.jobPosition} 
+         cName={position.clubName} 
+         eMail={position.email}></PositionCard>
       </Grid>
     ));
   }
