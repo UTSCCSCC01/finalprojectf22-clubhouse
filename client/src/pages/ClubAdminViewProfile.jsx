a6d62d6e8ca79c595265ff9a8d03f9fd4d0ba2be
amym-li <amyamay22@gmail.com>
2022-11-15 02:13:28 -0500

    MIN-33 Modify club profile viewing page

--- client/src/pages/ClubAdminViewProfile.jsx
+++ client/src/pages/ClubAdminViewProfile.jsx
@@ -1,154 +1,134 @@
 import React from "react";
 import MultiActionAreaCard from "./EventCard.jsx";
-import EventCard from "./StudentEventCard.jsx";
-import { useEffect,useState } from 'react';
+import EventCard from "./EventCard.jsx";
+import { useEffect, useState } from 'react';
 import InfiniteScroll from "react-infinite-scroll-component";
-import Fab from '@mui/material/Fab'
+import { IconButton, Box, CardMedia, Typography,Card, CardContent, Stack, Container, Grid } from '@mui/material/'
 import EditIcon from '@mui/icons-material/Edit';
 import { getCookie } from '../libraries/cookieDAO'
+import dateFormat from 'dateformat';
+
 /**
  * ClubAdminViewProfile
  * @component
  */
 function ClubAdminViewProfile(props) {
   const url = 'http://127.0.0.1:5001/club/events';
-  
+
   const clubName = getCookie("clubName");
   // console.log(props.values);
-  const [isMore,setisMore] = useState(true);
-  const [items, setItems ] = useState([]);
+  const [isMore, setisMore] = useState(true);
+  const [items, setItems] = useState([]);
   const [page, setPage] = useState(2);
-    /**
- * <Gets page 1 of events from the database>
- */
+  const now = new Date();
 
-  
+  /**
+* <Gets page 1 of events from the database>
+*/
+
+
   useEffect(() => {
-  const getevents = async ()=>{
+    const getevents = async () => {
 
-  const res = await fetch(url+"?page=1"+ "&clubName=" + clubName);
+      const res = await fetch(url + "?page=1" + "&clubName=" + clubName);
 
-    const data = await res.json();
-    setItems(data);
-  };
-  
+      const data = await res.json();
+      setItems(data);
+    };
+
     getevents();
-  },[]);
+  }, []);
 
- 
+
   /**
  * <function description>
 
  * @returns  {Promise<Object>}        <returns the events depending on the page number from the database>
  */
-    const fetchData = async() => {
+  const fetchData = async () => {
 
-     const res = await fetch(url+'?page='+page+"&clubName="+clubName);
+    const res = await fetch(url + '?page=' + page + "&clubName=" + clubName);
 
-      const data = await res.json();
-      
-      return data;
-    };
-/**
- * < increments the page number by 1 and if there are no events left it sets the variable isMore to false>
- * 
- */
-    const fetchd = async () => {
-      const eventfromserv = await fetchData();
-    
-      setItems([...items, ...eventfromserv]);
-      if(eventfromserv.length === 0 || eventfromserv.length <1){
-        setisMore(false);
-      }
-      
-      setPage(page+1);
+    const data = await res.json();
+
+    return data;
+  };
+  /**
+   * < increments the page number by 1 and if there are no events left it sets the variable isMore to false>
+   * 
+   */
+  const fetchd = async () => {
+    const eventfromserv = await fetchData();
+
+    setItems([...items, ...eventfromserv]);
+    if (eventfromserv.length === 0 || eventfromserv.length < 1) {
+      setisMore(false);
     }
-    const [image,setImage] = useState('');
 
-    /**
- * <fetches the image base64 string and sets image to that>
- * 
- */
-    useEffect(() => {
-      const fetchImage = async()=>{
-        const res = await fetch("http://127.0.0.1:5001/club/profileimg/" + clubName);
-        const data = await res.json();
-        
-        setImage(data);
-        
-        
-      }
-      fetchImage();
-    });
-    
+    setPage(page + 1);
+  }
+  const [image, setImage] = useState('https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Utoronto_coa.svg/1200px-Utoronto_coa.svg.png');
+
+  /**
+* <fetches the image base64 string and sets image to that>
+* 
+*/
+  useEffect(() => {
+    const fetchImage = async () => {
+      const res = await fetch("http://127.0.0.1:5001/club/profileimg/" + clubName);
+      const data = await res.json();
+
+      setImage(data);
+
+
+    }
+    fetchImage();
+  });
+
   return (
-    
-    <div class="mui-container-fluid" className="ClubAdminProfilePage">
-      <h1 style={{ textAlign: "center" }}> Profile</h1>
-      
 
-     <img
-            className="img2"
-            src={image}
-            alt="profile-picture"
-          />
-           
-      <div className="adminchild" >
-        
-      <div className="info">
-      <Fab  aria-label="edit"
-          className="profileEditButton"
-          type="submit"
-          color="primary"
+    <Stack mt="120px" mb="60px" ml="auto" mr="auto" alignItems="center" width="80%" minWidth="700px" maxWidth="1115px">
+      <Card sx={{ mb: "40px", width: "100%" }} raised>
+        <CardMedia
+          component="img"
+          height="250"
+          sx={{ objectFit: "contain" }}
+          image={image} alt="profile-picture" />
+
+        <CardContent sx={{padding: "32px"}}>
+          <Box display="flex" justifyContent="space-between">
+            <Typography flexWrap="wrap" gutterBottom variant="h4">{props.values.clubName}</Typography>
+            <Box width="20%" flex={0}>
+              <IconButton size="large" onClick={props.onClick}><EditIcon /></IconButton>
+            </Box>
+          </Box>
           
-          onClick={props.onClick}>
-        <EditIcon />
-      </Fab>
-      <h2  style={{ textAlign: "center" }}>{props.values.clubName}</h2>
+          <Typography gutterBottom variant="h6">Contact Info</Typography>
+          <Typography>Email: {props.values.email}</Typography>
+          <Typography gutterBottom>Phone: {props.values.clubPhone ? props.values.clubPhone : "N\\A"}</Typography>
 
-          
-          <div color="#FFE498" className="viewdesc" style = {{float: "right", width:"55%", margin:"5px", display: "inline-block", padding:"5px", marginTop:"50px", marginRight:"0px"}}>
-        
-        <p style={{paddingLeft:"5px"}}>{props.values.clubDesc}</p>
-        </div>
+          <Typography gutterBottom variant="h6">Description: </Typography>
+          <Typography>{props.values.clubDesc}</Typography>
+        </CardContent>
+      </Card>
 
-        <div className="contactinfo">
-        <h3> Contact Info:</h3>
-          <p > Email: {props.values.email}</p>
-          <p> Phone Number: {props.values.clubPhone} </p>
-          </div>
-      </div>
+      <Box sx={{mt: "24px", width: "100%"}}>
+        <Typography alignSelf="left" variant="h4" mb="24px">My Upcoming Events</Typography>
+        <Grid spacing={4} container>
+          {items && items.filter(item => item.eventStartTime >= dateFormat(now, "isoDateTime")).map((item) => {
+              return (<Grid item key={item}>
+                <EventCard key={item._id} eKey={item._id} cName={item.clubName} eName={item.eventName} eDate={item.eventDate} eJoin={item.eventJoin} eImage={item.eventImage} eStartTime={item.eventStartTime} eEndTime={item.eventEndTime} eLoc={item.eventLoc} eTags={item.eventTags} eDesc={item.eventDesc} />
+              </Grid>)
+            }
+          )}
 
-      <div className="cardcont">
-      <h2 style={{ textAlign: 'center' }}>My Events</h2>
-      <div id="scdiv" className="evenCard">
-      
-      <InfiniteScroll  
-      dataLength={items.length} //This is important field to render the next data
-      next={fetchd}
-     hasMore={isMore}
-     scrollableTarget="scdiv"
-      loader={<h4>Loading...</h4>}
-      endMessage={
-    <p style={{ textAlign: 'center' }}>
-      <b>Yay! You have seen it all</b>
-    </p> }>
-        {items.map((item) => {
-            return <EventCard key={item._id} cName={item.clubName} eName={item.eventName} eDate={item.eventDate} eJoin={item.eventJoin} eImage={item.eventImage} eStartTime={item.eventStartTime} eEndTime={item.eventEndTime} eLoc={item.eventLoc} eTags={item.eventTags} eDesc={item.eventDesc}/>
-        }
-        ) }
-      </InfiniteScroll>
-      </div>
+        </Grid>
+      </Box>
 
+    </Stack>
 
-   </div>
-   </div>
-      
-      
-    </div>
-    
   );
-}  
+}
 
 
 export default ClubAdminViewProfile;
--- client/src/pages/EventCard.jsx
+++ client/src/pages/EventCard.jsx
@@ -1,41 +1,139 @@
 import * as React from 'react';
-import axios from 'axios';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
-import { useEffect,useState } from 'react';
+import dateFormat from 'dateformat';
+import EventIcon from '@mui/icons-material/Event';
+import TimeIcon from '@mui/icons-material/AccessTime';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { CardActions, Box } from '@mui/material';
+import { useState } from 'react';
+import EventTag from "./EventTag.jsx"
+import { styled } from '@mui/material/styles';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
+import { getCookie } from '../libraries/cookieDAO'
+import CloseIcon from '@mui/icons-material/Close';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import PropTypes from 'prop-types';
 
-/**
- * @component
- */
 
-function MultiActionAreaCard(props) {
-  
-  const [eName, setEname] = useState('');
-  const [eDesc, setEdesc] = useState('');
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
+function BootstrapDialogTitle(props) {
+  const { children, onClose, ...other } = props;
 
   return (
-    <Card className='card'>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg"
-          alt=""
-        />
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+}
+
+BootstrapDialogTitle.propTypes = {
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+};
+
+/**
+ * Display fetched information in a card
+ * Reformat eventStartTime and eventEndTime. 
+ * @param {*} props 
+ * @component
+ */
+export default function EventCard(props) {
+
+  const [expanded, setExpanded] = React.useState(false);
+  const [OnOff, setOnOff] = useState(false);
+  const user = getCookie("username");
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  function handleClick() {
+    setOnOff(!OnOff);
+  }
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card raised sx={{ width: 350 }} >
+      <CardMedia
+        component="img"
+        height="250"
+        image={props.eImage} alt="event image" />
+      <CardContent sx={{ width: 370 }}>
+        <Typography gutterBottom variant="h5" component="h2">{props.eName}</Typography>
+        <Typography><EventIcon fontSize="inherit" ></EventIcon>  {dateFormat(props.eStartTime, "mmmm dS, yyyy")} </Typography>
+        <Typography><TimeIcon fontSize="inherit"></TimeIcon> {dateFormat(props.eStartTime, "shortTime")}</Typography>
+        <Typography>  <LocationOnIcon fontSize="inherit"></LocationOnIcon> {props.eLoc} </Typography>
+
+      </CardContent>
+
+      <CardActions disableSpacing>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="more info"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {props.eName}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {props.eDesc}
-          </Typography>
+          <Typography sx={{m: "0 4px 0 4px"}} paragraph>{props.eDesc}</Typography>
+
+          <Box display="inline-flex" flexWrap="wrap" mt="20px">
+            {(props.eTags).map((tag) => (
+              <EventTag data={tag} />
+            ))}
+          </Box>
         </CardContent>
-      </CardActionArea>
-    
+      </Collapse>
     </Card>
   );
 }
-export default MultiActionAreaCard;
