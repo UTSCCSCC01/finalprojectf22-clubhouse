44d125f4a852a7527efb8986e4c31e2f90c78fa1
amym-li <amyamay22@gmail.com>
2022-11-18 12:29:31 -0500

    MIN-33 Display incorrect login info message instead of alert

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { TextField, Container, Typography, Grid, Button } from '@material-ui/core'
 import Auth from '../components/AuthCheck.jsx';
 import '../styles.css';
+import Alert from '@mui/material/Alert';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
 /**
  * A component storing the Login form.
@@ -14,6 +18,8 @@ class Login extends Component {
     this.state = {
       email: '',
       password: '',
+      open: false,
+      submitStatus: "error"
     };
     this.submit = this.submit.bind(this);
     this.change = this.change.bind(this);
@@ -58,7 +64,8 @@ class Login extends Component {
             location.href = "http://localhost:3000/SCSUClubs";
           }
         } else {
-          alert("Incorrect Password!")
+          this.setState({ open: true, submitStatus:  "error"})
+          // alert("Incorrect Password!")
         }
       });
   }
@@ -80,6 +87,26 @@ class Login extends Component {
                 <Button variant="contained" color="primary" style={{ width: 350, marginTop: 25 }} type="submit">Login</Button>
               </Grid>
             </Grid>
+            <Collapse sx={{ mt: 3, width: "100%"}} in={this.state.open}>
+                <Alert
+                    severity={this.state.submitStatus}
+                    action={
+                        <IconButton
+                            aria-label="close"
+                            color="inherit"
+                            size="small"
+                            onClick={() => {
+                                this.setState({open: false});
+                            }}
+                        >
+                            <CloseIcon fontSize="inherit" />
+                        </IconButton>
+                    }
+                    sx={{ mb: 2 }}
+                >
+                    {this.state.submitStatus === "error" && "Incorrect email or password."}
+                </Alert>
+            </Collapse>
           </Container>
         </form>
       </div>
