e801645e700f6757cc14530be69ab05a84671e4a
amym-li <amyamay22@gmail.com>
2022-11-18 08:08:21 -0500

    MIN-33 Add confirmation to new announcement page

--- client/src/pages/NewAnnouncement.jsx
+++ client/src/pages/NewAnnouncement.jsx
@@ -6,6 +6,10 @@ import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined
 import { useNavigate } from 'react-router';
 import dayjs from "dayjs";
 import { getCookie } from '../libraries/cookieDAO'
+import Alert from '@mui/material/Alert';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
 /**
  * Component for displaying a new announcements form
@@ -20,11 +24,19 @@ const NewAnnouncement = () => {
     const [message, setMessage] = useState("");
     const [recipients, setRecips] = useState("everyone");
 
+    const [open, setOpen] = useState(false);
+    const [submitStatus, setStatus] = useState(""); // "success" or "error"
+
+    const clearForm = () => {
+        setSub("");
+        setMessage("");
+    }
+
     /**
      * Write the announcement to the database and redirect to the home page
      * @param {Event} e 
      */
-    
+
     const onSubmit = (e) => {
         e.preventDefault();
 
@@ -38,11 +50,15 @@ const NewAnnouncement = () => {
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(newAnnouncement)
         }).then(() => {
-
+            setStatus("success");
+            setOpen(true);
         }).catch((err) => {
             console.log(err);
+            setStatus("error");
+            setOpen(true);
         })
-        navigate("/"); // change path
+
+        clearForm();
     };
 
     return (
@@ -61,6 +77,7 @@ const NewAnnouncement = () => {
                 fullWidth
                 variant="outlined"
                 label="Subject"
+                value={subject}
                 onChange={(e) => setSub(e.target.value)}
                 required
             ></TextField>
@@ -70,12 +87,13 @@ const NewAnnouncement = () => {
                 multiline
                 variant="outlined"
                 minRows={4}
+                value={message}
                 required
                 onChange={(e) => setMessage(e.target.value)}
                 label="Message"
             ></TextField>
 
-            <FormControl sx={{marginLeft: "0", marginRight: "auto"}}>
+            <FormControl sx={{ marginLeft: "0", marginRight: "auto" }}>
                 <FormLabel>Notify</FormLabel>
                 <RadioGroup
                     row
@@ -91,15 +109,15 @@ const NewAnnouncement = () => {
 
             <Box sx={{ width: "100%", display: 'flex', justifyContent: "space-between" }}>
                 <Button
-                    sx={{width: "120px"}}
-                    onClick={() => navigate("/")} // change path
+                    sx={{ width: "120px" }}
+                    onClick={clearForm}
                     variant="contained"
                     color="secondary"
                     endIcon={<DeleteForeverOutlinedIcon />}
                 >Cancel</Button>
                 <Button
                     onClick={onSubmit}
-                    sx={{width: "120px"}}
+                    sx={{ width: "120px" }}
                     type="submit"
                     variant="contained"
                     color="primary"
@@ -107,6 +125,26 @@ const NewAnnouncement = () => {
                 >Send</Button>
             </Box>
 
+            <Collapse sx={{width: "100%"}} in={open}>
+                <Alert
+                    severity={submitStatus}
+                    action={
+                        <IconButton
+                            aria-label="close"
+                            color="inherit"
+                            size="small"
+                            onClick={() => {
+                                setOpen(false);
+                            }}
+                        >
+                            <CloseIcon fontSize="inherit" />
+                        </IconButton>
+                    }
+                    sx={{ mb: 2 }}
+                >
+                    {submitStatus === "success" ? "Announcement sent." : "Unable to send."}
+                </Alert>
+            </Collapse>
         </Box>
     );
 }
